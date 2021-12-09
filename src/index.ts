158591b581557ac6cb245153b9dcd28cd326cbf2
vagnerngoulart <vagnerngoulart@hotmail.com>
2021-12-09 20:35:00 -0300

    Teste

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,10 @@ import express from "express";
 
 const app = express();
 
+
 app.use(express.json());
 
-const port = 5000;
+const port = 5000; 
 
 app.get('/', (req, res) => {
     res.status(200).json({
